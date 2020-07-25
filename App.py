9777b95d72c47393e4f7b3d961dd45f501cc8cd5
vengle <victor.engle@gmail.com>
2020-07-25 11:52:23 +0000

    Comments

--- App.py
+++ App.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# This is just the minimum flask framework needed to use render_template in a standalone script.
+# This project uses render_template to create static pages with charts of covid data,
+
 @app.route('/')
 def hello():
     return "Hello World!"
